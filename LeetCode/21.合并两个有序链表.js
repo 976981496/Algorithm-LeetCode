1bd7b79db61950c2948058885e2e0257dbee2307
lihaiyan <lihaiyan@linklogis.com>
2022-12-29 10:57:45 +0800

    算法

--- LeetCode/21.合并两个有序链表.js
+++ LeetCode/21.合并两个有序链表.js
@@ -64,7 +64,23 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-  const name = new ListNode();
+    const head = new ListNode();
+    if ( list1== null) {
+        return list2
+    }
+    if (list2 == null) {
+        return list1
+    }
+    if (list1.val <= list2.val) {
+        
+        list1.next=mergeTwoLists(list1.next,list2)
+        
+        return list1
+    } else {
+        list2.next=mergeTwoLists(list1,list2.next)
+        return list2
+    }
+    
 
 };
 // @lc code=end
